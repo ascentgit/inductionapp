17ea924b90d94102adf10520183716cea176f248
Sai Parvathi Veeramani <saiparvathiv@gmail.com>
2019-05-19 04:32:21 -0700

    Initail Upload

--- app/models/config.js
+++ app/models/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'mongosession'				: process.env.MONGODB_SESSION,
     'authentication'			: process.env.MONGODB_USR,
-	'customerdata'				: process.env.MONGODB_CUSTOMER_DATA
+	'company'					: process.env.MONGODB_COMPANY_DATA,
 };
